96ee43e4d36abb9f263803b2d69aa4d57c02c2bb
Erik Ogan <erik@ogan.net>
2017-05-05 10:46:48 -0700

    Dirt simple Sinatra selector app

---
+++ editor.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'pathname'
+
+PUBLIC_FOLDER='images'
+set :root, File.dirname(__FILE__)
+set :public_folder, Proc.new { File.join(root, PUBLIC_FOLDER) }
+
+get '/' do
+  files = Dir["#{PUBLIC_FOLDER}/*.{png,gif,jpg}"].sort
+  @files = files.map do |f|
+    base = f.gsub(%r{.*/}, '')
+    name = base.gsub(/\.\w+$/, '')
+    {
+      base: base,
+      name: name,
+      url: base,
+    }
+  end
+  haml :index, :format => :html5
+end
+
+post '/' do
+  files = params.keys.reject {|k| k == 'submit'}
+  files.each do |f|
+    File.rename("#{PUBLIC_FOLDER}/#{f}", "#{PUBLIC_FOLDER}/removed/#{f}")
+  end
+
+  redirect '/'
+end
