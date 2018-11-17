1f47a6cda5ff59e3828f1320b8831c9181db6ccb
Erik Ogan <erik@ogan.net>
2018-11-17 10:04:39 -0800

    refactor compare_images.rb to make Rubocop happy

--- compare_images.rb
+++ compare_images.rb
@@ -14,35 +14,54 @@ class Fixer
   include Util
 
   def run
-    source_data = @source.cached_data_and_images
-    dest_data = @dest.cached_data_and_images
-    byebug; 1
     diffs = []
 
-    dest_data.each do |name, data|
-      next if data[:alias]
-
-      source = source_data[name]
-      next unless source
-
-      source = source_data[source[:alias]] while source.key?(:alias)
-
-      if data[:md5] == source[:md5]
+    @dest_data.each do |name, data|
+      if diff_item(name, data)
+        diffs << name
+        puts "#{name} : DIFFERENT"
+      else
         puts "#{name} : same"
-        next
       end
-
-      diffs << name
-      puts "#{name} : DIFFERENT"
     end
 
-    puts diffs.to_yaml
-    File.write("data/diff.#{@dest.name}.yml", diffs.to_yaml)
+    write_data(diffs)
+  end
+
+  def diff_item(name, data)
+    return false if data[:alias]
+
+    source = source_for_name(name)
+    return false unless source
+
+    if data[:md5] == source[:md5]
+      puts "#{name} : same"
+      return false
+    end
+
+    true
+  end
+
+  def source_for_name(name)
+    source = @source_data[name]
+    return nil unless source
+
+    source = @source_data[source[:alias]] while source.key?(:alias)
+    source
+  end
+
+  def write_data(data)
+    yaml = data.to_yaml
+    puts yaml
+    File.write("data/diff.#{@dest.name}.yml", yaml)
   end
 
   def initialize(dest, source = nil)
     @dest = configs[dest.to_sym] || raise("no such workspace: #{dest}")
     @source = source ? configs[source.to_sym] || raise("no such workspace #{source}") : configs[:_source]
+
+    @dest_data = @dest.cached_data_and_images
+    @source_data = @source.cached_data_and_images
   end
 end
 
