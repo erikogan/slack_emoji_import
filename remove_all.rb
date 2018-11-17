cea47cda793a4cb1a714b3e584e727c5926bb245
Erik Ogan <erik@ogan.net>
2018-11-17 09:10:44 -0800

    refactor remove_all.rb to make Rubocop happy
    Also: I think the original version was removing all emoji from all configs all at once

--- remove_all.rb
+++ remove_all.rb
@@ -14,22 +14,35 @@ class Fixer
     emoticons.each do |emoticon|
       puts "============== #{emoticon}"
       configs.each do |name, config|
-        next if name == :_source || config == configs[:_source]
+        next if source?(config)
+        next unless check_presence(emoticon, config)
+        next unless do_remove(emoticon, config)
 
-        unless config.cached_data['emoji'][emoticon]
-          puts "Not in #{name}, skipping"
-          next
-        end
-        begin
-          config.remove_emoji(emoticons)
-        rescue Selenium::WebDriver::Error::TimeOutError
-          puts "error on #{name}, moving on"
-          next
-        end
         puts name
         sleep 1
       end
     end
+  end
+
+  def source?(config)
+    config.name == :_source || config == configs[:_source]
+  end
+
+  def check_presence(emoticon, config)
+    present = !config.cached_data['emoji'][emoticon].nil?
+    puts "Not in #{config.name}, skipping" unless present
+    present
+  end
+
+  def do_remove(emoticon, config)
+    begin
+      config.remove_emoji(emoticon)
+    rescue Selenium::WebDriver::Error::TimeOutError
+      puts "error on #{config.name}, moving on"
+      return false
+    end
+
+    true
   end
 end
 
